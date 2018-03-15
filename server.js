b982feb12e557701514cf71cdf0db469153fb307
B Hicks <b@audiblePi.com>
2018-03-14 23:10:41 -0400

    updated default path

--- server.js
+++ server.js
@@ -66,23 +66,25 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 //   });
 // };
 
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  // if (!db) {
-  //   initDb(function(err){});
-  // }
-  // if (db) {
-  //   var col = db.collection('counts');
-  //   // Create a document with request IP and current time of request
-  //   col.insert({ip: req.ip, date: Date.now()});
-  //   col.count(function(err, count){
-  //     res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-  //   });
-  // } else {
-    res.render('index.html', { pageCountMessage : null});
-  //}
-});
+app.get('/', (req, res) => res.json({ message: 'telemetry data stream demo' }) );
+
+// app.get('/', function (req, res) {
+//   // try to initialize the db on every request if it's not already
+//   // initialized.
+//   // if (!db) {
+//   //   initDb(function(err){});
+//   // }
+//   // if (db) {
+//   //   var col = db.collection('counts');
+//   //   // Create a document with request IP and current time of request
+//   //   col.insert({ip: req.ip, date: Date.now()});
+//   //   col.count(function(err, count){
+//   //     res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+//   //   });
+//   // } else {
+//     res.render('index.html', { pageCountMessage : null});
+//   //}
+// });
 
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
